8ee092b1bbf5f880716c47a348396958520cba76
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-08-26 20:21:40 -0400

    Add Tweet model

---
+++ models/Tweet.js
@@ -0,0 +1,20 @@
+// models/Tweet.js
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const tweetSchema = new Schema({
+  user:       { type : Schema.Types.ObjectId, ref: 'Users', required: true },
+  date:       { type: Date, default: Date.now },
+  hashtags:   { type: Array, default: [] },
+  message:    { type: String },
+  retweetId:  { type: String, default: '' },
+  replayId:   { type: String, default: '' },
+  retweets:   { type: Number, default: 0 },
+  likes:      { type: Number, default: 0 },
+}, {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+});
+
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+module.exports = Tweet;
